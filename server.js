1c4af1465ea2c17c88ddc86d3f8c3bc6cc9d7cb2
shiron manuelpillai <shironm1993@gmail.com>
2018-09-04 14:52:44 +0100

    npm start

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -56,6 +57,6 @@ app.get('/bad', (req,res) => {
     })
 });
 //port to listen too
-app.listen(3000, () =>{
-    console.log('server is running')
+app.listen(port, () =>{
+    console.log(`server is running on ${port}`)
 });
